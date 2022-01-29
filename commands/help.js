15f75730a327677e4c6d2f17c279e59f5dd57d00
zatzk <zatzk@outlook.com>
2022-01-29 12:44:22 -0300

    added more content to !help description

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = async function (msg, args) {
 
   const embed = new MessageEmbed()
     .setTitle('Lista de Comandos')
-    .setDescription("```!help + [comando] para detalhes``` ```!gif + [search]``` ```!baka``` ```!cry``` ```!cuddle``` ```!feed``` ```!hug``` ```!kiss``` ```!laugh``` ```!lick``` ```!pat``` ```!poke``` ```!slap``` ```!smug``` ```!tickle```")
+    .setDescription("```!help + [comando]``` ```!gif + [search]``` ```!baka``` ```!cry``` ```!cuddle``` ```!feed``` ```!hug``` ```!kiss``` ```!laugh``` ```!lick``` ```!pat``` ```!poke``` ```!slap``` ```!smug``` ```!tickle```")
     .setColor("RANDOM")
 
   msg.channel.send({ embeds: [embed] });
