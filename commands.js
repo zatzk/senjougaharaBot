d6aa11365f3614d242c0c576b452f2c521e1c4c0
zatzk <zatzk@outlook.com>
2022-01-29 17:42:19 -0300

    prefix change

--- commands.js
+++ commands.js
@@ -24,7 +24,7 @@ module.exports = async function(msg) {
   let command = tokens.shift();
   
 
-  if(command.charAt(0) ==='!') {
+  if(command.charAt(0) ==='_') {
     command = command.substring(1);
     
 
--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = async function (msg, args) {
 
   const embed = new MessageEmbed()
     .setTitle('Lista de Comandos')
-    .setDescription("```!help + [comando]``` ```!gif + [search]``` ```!baka``` ```!cry``` ```!cuddle``` ```!feed``` ```!hug``` ```!kiss``` ```!laugh``` ```!lick``` ```!pat``` ```!poke``` ```!slap``` ```!smug``` ```!tickle```")
+    .setDescription("```_help + [comando]``` ```_gif + [search]``` ```_baka``` ```_cry``` ```_cuddle``` ```_feed``` ```_hug``` ```_kiss``` ```_laugh``` ```_lick``` ```_pat``` ```_poke``` ```_slap``` ```_smug``` ```_tickle```")
     .setColor("RANDOM")
 
   msg.channel.send({ embeds: [embed] });
