c7c2a4bdf697d49065f01af33c6209c84823f43e
Jean Luiz <zatzk@outlook.com>
2022-03-09 21:48:47 -0300

    music help commands

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -6,9 +6,9 @@ module.exports = async function (msg, args) {
 
   const embed = new MessageEmbed()
     .setTitle('🚩 Comandos 🚩')
-    .addField('**🍕 Misc 🍕**', '`gif`')
+    .addField('**🍕 Misc 🍕**', '`help`,`gif`')
     .addField('**✨ Fun ✨**', '`gatinho`, `cuddle`, `feed`, `baka`, `hug`, `cry`, `kiss`, `laugh`, `lick`, `pat`, `poke`, `slap`, `smug`, `tickle`')
-    .addField('**🎵 Music 🎵**', '`--`')
+    .addField('**🎵 Music 🎵**', '`play`, `pause`, `queue`, `resume`, `skip`, `stop`, `leave`')
     .setColor("RANDOM")
 
   msg.channel.send({ embeds: [embed] });
