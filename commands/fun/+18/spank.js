a785b8053dd3b8b50fa1e59a88ffa3fa82b91e30
Jean Luiz <zatzk@outlook.com>
2022-04-24 10:48:59 -0300

    adicionado comando +18 spank, corrigido comando kiss

--- commands/fun/+18/spank.js
+++ commands/fun/+18/spank.js
@@ -1,27 +1,29 @@
 const fetch = require('node-fetch');
 const { MessageEmbed } = require('discord.js');
-const neko = require('../../exports/nekoExports/neko.js');
+const neko = require('../../../exports/nekoExports/neko.js');
 
 module.exports = async function (msg, args) {
-  let keywords = 'kiss';
+  let keywords = 'spank';
   let url = `http://api.nekos.fun:8080/api/${keywords}`
   let response = await fetch(url);
 
-  const json = [await response.json(), await neko.sfw.kiss()]
+  const json = [await response.json(), await neko.nsfw.spank()]
   json[0] = json[0].image;
   json[1] = json[1].url;
 
   random = Math.floor(Math.random() * 2);
-
+  
   if(args.toString().charAt(1) === '@'){
     const embed = new MessageEmbed()
-      .setDescription(`**${msg.author.username}** deu beijinho em ${args}`)
+      .setDescription(`**${msg.author.username}** deu umas palmadinhas em ${args} (͡ ° ͜ʖ ͡ °)`)
       .setImage(json[random])
     msg.channel.send({ embeds: [embed] });
   }
+
   else{
     const embed = new MessageEmbed()
-      .setDescription(`tem ninguém pra beijar né? (ʘ ͜ʖ ʘ)`)
+      .setDescription(`(¬‿¬)`)
+      .setImage(json[random])
     msg.channel.send({ embeds: [embed] });
   }
   
