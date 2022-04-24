71f019d2e6225e6a21abc527dbc838677ab12f5c
Jean Luiz <zatzk@outlook.com>
2022-04-24 10:14:17 -0300

    adicionado novo comando tr e footer no commando help

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -1,15 +1,14 @@
-const fetch = require('node-fetch');
 const { MessageEmbed } = require('discord.js');
 
 
 module.exports = async function (msg, args) {
-
   const embed = new MessageEmbed()
     .setTitle('🚩 Comandos 🚩')
-    .addField('**🍕 Misc 🍕**', '`help`,`gif`')
+    .addField('**🍕 Misc 🍕**', '`help`,`gif`,`tr`')
     .addField('**✨ Fun ✨**', '`gatinho`, `cuddle`, `feed`, `baka`, `hug`, `cry`, `kiss`, `laugh`, `lick`, `pat`, `poke`, `slap`, `smug`, `tickle`')
     .addField('**🎵 Music 🎵**', '`play`, `pause`, `queue`, `resume`, `skip`, `stop`, `leave`')
-    .setColor("RANDOM")
+    .setColor("#FF7F30")
+    .setFooter({text:` Página com todos os comandos em breve 🚧 `})
 
   msg.channel.send({ embeds: [embed] });
 }
